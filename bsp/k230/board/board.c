7cb0b253545f8225645f1840a3ae43e77c8b1a90
Wang Chen <unicorn_wang@outlook.com>
2025-04-02 15:48:51 +0800

    bsp: k230: use sysctl to reboot the board
    Signed-off-by: Wang Chen <unicorn_wang@outlook.com>

--- bsp/k230/board/board.c
+++ bsp/k230/board/board.c
@@ -19,6 +19,8 @@
 
 #include <sbi.h>
 
+#include "sysctl_boot.h"
+
 #ifdef RT_USING_SMART
 #include <mmu.h>
 #include "page.h"
@@ -113,7 +115,8 @@ void rt_hw_board_init(void)
 
 void rt_hw_cpu_reset(void)
 {
-    sbi_shutdown();
+    // sbi_shutdown();
+    sysctl_boot_reset_soc();
     while(1);
 }
 MSH_CMD_EXPORT_ALIAS(rt_hw_cpu_reset, reboot, reset machine);
